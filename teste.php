72b119ed28ad696d0dc43c3e8c3f46deaccde039
Rafael Guzzo <rafaajuda@hotmail.com>
2019-07-26 21:04:00 -0300

    site categorias

--- teste.php
+++ teste.php
@@ -1,25 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+    <form action="" method="post" enctype="multipart/form-data">
+    <input type="file" name="arquivo[]" multiple="multiple">
+    <input type="submit" value="enviar">
+    </form>
+</body>
+</html>
+
 <?php 
 require_once("vendor/autoload.php");
 
 use \Hcode\DB\Sql;
 Use \Hcode\Model;
+$files = $_FILES["arquivo"];
+echo "<pre>";
+print_r($files);
+echo "</pre>";
 
-
-
-$sql = new Sql();
-
-$results = $sql->select("select * from tb_users");
-
-$data = $results[0];
-
-array_push($data, ["outroArray" => $results[1]]);
-
-$a = new Model();
-$a->setData($data);
-var_dump($data);
-
-foreach ($data as $key => $value) {
-    echo "<br>Key = " .$key ." value " .var_dump($value);
+foreach ($files as $key => $value) {
+    # code...
 }
+
 
 ?>
